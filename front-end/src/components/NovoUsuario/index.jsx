a6d71ac5cdc4c1539e07f31f4a5141659a711448
Arthur Freitas <arthurssfreitas@gmail.com>
2020-02-09 15:54:17 -0300

    adding tags in component NewUser

--- front-end/src/components/NovoUsuario/index.jsx
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Label from '../Label';
 
 export class NovoUsuario extends Component {
   render() {
     return (
       <div className="center">
+        <form>
+          <Label htmlFor="nome" texto="Quem é você?"/>
+        </form>
       </div>
     );
   }
